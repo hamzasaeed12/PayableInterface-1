a01812880f440e2c61386c58c53ec566d227453f
eke1 <eke1@umbc.edu>
2016-11-28 23:02:38 -0500

    Updated PayableInterfaceTest

--- PayableInterfaceTest.java
+++ PayableInterfaceTest.java
@@ -1,4 +1,3 @@
-// Fig. 10.15: PayableInterfaceTest.java
 // Tests interface Payable.
 
 public class PayableInterfaceTest 
@@ -9,15 +8,12 @@ public class PayableInterfaceTest
       Payable payableObjects[] = new Payable[ 4 ];
       
       // populate array with objects that implement Payable
-      payableObjects[ 0 ] = new Invoice( "01234", "seat", 2, 375.00 );
-      payableObjects[ 1 ] = new Invoice( "56789", "tire", 4, 79.95 );
-      payableObjects[ 2 ] = 
-         new SalariedEmployee( "John", "Smith", "111-11-1111", 800.00 );
-      payableObjects[ 3 ] = 
-         new SalariedEmployee( "Lisa", "Barnes", "888-88-8888", 1200.00 );
+      payableObjects[ 0 ] = new Invoice( "01234", "pc", 2, 399.99 );
+      payableObjects[ 1 ] = new Invoice( "56789", "first aid kit", 4, 100.00 );
+      payableObjects[ 2 ] = new SalariedEmployee( "Liz", "","Brown", "443-892-1727", 800.00 );
+      payableObjects[ 3 ] = new SalariedEmployee( "David", "Adam", "Bowen", "585-24-7463", 1200.00 );
 
-      System.out.println( 
-         "Invoices and Employees processed polymorphically:\n" ); 
+      System.out.println( "Processed Invoices and Employees:\n" ); 
 
       // generically process each element in array payableObjects
       for ( Payable currentPayable : payableObjects )
@@ -25,23 +21,7 @@ public class PayableInterfaceTest
          // output currentPayable and its appropriate payment amount
          System.out.printf( "%s \n%s: $%,.2f\n\n", 
             currentPayable.toString(), 
-            "payment due", currentPayable.getPaymentAmount() ); 
-      } // end for
-   } // end main
-} // end class PayableInterfaceTest
-
-
-/**************************************************************************
- * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
+            "Payment", currentPayable.getPaymentAmount() ); 
+      } 
+   }
+} 
